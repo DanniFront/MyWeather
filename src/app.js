dd6fcf1389d623975527698812e16c9dc765ef3b
DanniFront <mure.daniela@gmail.com>
2024-11-14 13:45:47 +0100

    Added API key

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+function searchCity(city) {
+  let apiKey = "748d5734be19te1af0e3a12aa9abaofd";
+}
+
 function citySearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
