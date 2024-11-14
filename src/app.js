06594dd45d86b0709b4c4e90bb73b268691841d7
DanniFront <mure.daniela@gmail.com>
2024-11-14 14:34:16 +0100

    Added the search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "748d5734be19te1af0e3a12aa9abaofd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
 }
 
 function citySearchSubmit(event) {
@@ -12,3 +22,5 @@ function citySearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", citySearchSubmit);
+
+searchCity("Helsingborg");
